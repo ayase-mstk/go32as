4dc253955a699d8d3d1f4880c3bc72363a56eed4
ayase-mstk <masa1215.hayase@gmail.com>
2024-09-18 23:12:20 +0900

    :bug: add to void duplication in string/shstring table

--- src/elf32/shstringTable.go
+++ src/elf32/shstringTable.go
@@ -1,20 +1,29 @@
 package elf32
 
 type Elf32Shstrtbl struct {
-    data   []byte            // 連結された文字列のバッファ
-    idx    map[string]int    // 文字列からインデックスへのマッピング
+	data []byte         // 連結された文字列のバッファ
+	idx  map[string]int // 文字列からインデックスへのマッピング
+}
+
+func (s *Elf32Shstrtbl) exist(name string) bool {
+	_, exists := s.idx[name]
+	return exists
 }
 
 func (s *Elf32Shstrtbl) resolveIndex(name string) Elf32Word {
-    // 既に存在していれば追加する
-    if _, exist := s.idx[name]; exist {
-      return Elf32Word(s.idx[name])
-    }
+	// 既に存在していれば追加する
+	if s.exist(name) {
+		return Elf32Word(s.idx[name])
+	}
+	if s.idx == nil {
+		s.idx = make(map[string]int)
+	}
 
-    // 現在の文字列テーブルの末尾インデックスを取得
-    idx := len(s.data)
-    // シンボル名をnull終端付きで追加
-    s.data = append(s.data, []byte(name)...)
-    s.data = append(s.data, 0) // null終端
-    return Elf32Word(idx)
+	// 現在の文字列テーブルの末尾インデックスを取得
+	idx := len(s.data)
+	s.idx[name] = idx
+	// シンボル名をnull終端付きで追加
+	s.data = append(s.data, []byte(name)...)
+	s.data = append(s.data, 0) // null終端
+	return Elf32Word(idx)
 }
--- src/elf32/stringTable.go
+++ src/elf32/stringTable.go
@@ -1,20 +1,28 @@
 package elf32
 
 type Elf32Strtbl struct {
-    data   []byte            // 連結された文字列のバッファ
-    index  map[string]int    // 文字列からインデックスへのマッピング
+	data  []byte         // 連結された文字列のバッファ
+	index map[string]int // 文字列からインデックスへのマッピング
+}
+
+func (st *Elf32Strtbl) exist(name string) bool {
+	_, exists := st.index[name]
+	return exists
 }
 
 func (st *Elf32Strtbl) resolveIndex(name string) Elf32Word {
-    // 既に存在していれば追加する
-    if _, exist := st.index[name]; exist {
-      return Elf32Word(st.index[name])
-    }
+	if st.exist(name) {
+		return Elf32Word(st.index[name])
+	}
+	if st.index == nil {
+		st.index = make(map[string]int)
+	}
 
-    // 現在の文字列テーブルの末尾インデックスを取得
-    index := len(st.data)
-    // シンボル名をnull終端付きで追加
-    st.data = append(st.data, []byte(name)...)
-    st.data = append(st.data, 0) // null終端
-    return Elf32Word(index)
+	// 現在の文字列テーブルの末尾インデックスを取得
+	index := len(st.data)
+	st.index[name] = index
+	// シンボル名をnull終端付きで追加
+	st.data = append(st.data, []byte(name)...)
+	st.data = append(st.data, 0) // null終端
+	return Elf32Word(index)
 }
